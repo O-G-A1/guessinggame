980e3bde5d02b1fac2afa08c145a7dc937400804
Gbenga Ogunsola <gbengaadeboye99@gmail.com>
2024-07-28 08:06:17 +0100

    edited

--- assets/js/guessinggame.js
+++ assets/js/guessinggame.js
@@ -2,12 +2,11 @@ let correctGuess = parseInt(Math.random()*8)+1;
 let Guess;
 let minimumAttempt = 1;
 let maximumAttempt = 5;
-let reducedAttempt = 5;
 console.log(correctGuess);
 
 
 for(minimumAttempt=1; minimumAttempt<=maximumAttempt; minimumAttempt++){
-    Guess= parseInt(prompt("Guess " + minimumAttempt + " of " + maximumAttempt + ": Enter your guess (1-8):"), 10);
+    Guess= parseInt(prompt("Guess " + minimumAttempt + " of " + maximumAttempt + ": Enter your guess (1-8):"));
 
     if(Guess==correctGuess){
         alert("Congratulations! You guessed right, your guess is " + correctGuess + ", so this ends the game");
@@ -19,16 +18,16 @@ for(minimumAttempt=1; minimumAttempt<=maximumAttempt; minimumAttempt++){
     }
     
     // else if(Guess<correctGuess){
-    //     alert("Wrong guess, try again.");
-    // }
-    // else if(Guess>correctGuess){
-    //     alert("Wrong guess, try again.");
-    // }
-
-    if(minimumAttempt==maximumAttempt){
-        alert("Sorry, you have used all your attemts. You lose! ");
-    }
+        //     alert("Wrong guess, try again.");
+        // }
+        // else if(Guess>correctGuess){
+            //     alert("Wrong guess, try again.");
+            // }
+            
     let reducedAttempt=maximumAttempt-minimumAttempt;
+    if(minimumAttempt==maximumAttempt){
+        alert("Sorry, you have used all your attempts. You lose! ");
+    }
     if(reducedAttempt>0){
         alert("you have " + reducedAttempt + " attempts left");
     }
